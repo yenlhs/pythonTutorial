5e31a2295d1861db3b1e78ee439986434f803656
Adrian <yenlhs@gmail.com>
2018-01-23 23:24:29 +1100

    Successfully made mariadb connection
    Signed-off-by: Adrian <yenlhs@gmail.com>

--- database/mariaDBconn.py
+++ database/mariaDBconn.py
@@ -1,25 +1,43 @@
 import mysql.connector
-import configparser
+import configDB as cf
 from mysql.connector import errorcode
 
 
 
-try:
-    cnx = mysql.connector.connect(user='yen',password='Service1',host='10.1.1.9',database='eprog')
-    cur = cnx.cursor()
-    cur.execute("select * from test")
-    
-    for col1,col2 in cur:
-        print(col1,col2)
+def createConn():
+    #try:
+    #cnx = mysql.connector.connect(user=cf.username,password=cf.password,host=cf.host,database=cf.db)
+    cnx = mysql.connector.connect(user=cf.username,password=cf.password,host=cf.host,database=cf.db)
+    return cnx
+        # cur = cnx.cursor()
+        # cur.execute("select * from test")
+        # for col1,col2 in cur:
+        #     print(col1,col2)
 
-except mysql.connector.Error as err:
-    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-        print("Something is wrong with your username and password")
-    elif err.errno == errorcode.ER_BAD_DB_ERROR:
-        print("Database does not exist")
-    else:
-        print(err)
-else:
-    cnx.close()
+    # except mysql.connector.Error as err:
+    #     if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+    #         print("Something is wrong with your username and password")
+    #     elif err.errno == errorcode.ER_BAD_DB_ERROR:
+    #         print("Database does not exist")
+    #     else:
+    #         print(err)
+    # else:
+    #     cnx.close()
+
+a = createConn()
+cur = a.cursor()
+cur.execute("select * from test")        
+for col1,col2 in cur:
+    print(col1,col2)
+
+cur.close()
+createConn().close
+
+
+
+
+
+
+
 
 
