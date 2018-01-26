ddfbc70cbccc70583b9682ffcc32ffad74a18c54
Adrian <yenlhs@gmail.com>
2018-01-26 22:37:52 +1100

    used matplotlib

--- database/readfile.py
+++ database/readfile.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as mp
 
 #change this variable to adjust the number of rows to display. 
 #Write None if you wish to display all rows
@@ -13,7 +14,15 @@ csvfile = 'test.csv'
 #read csv file and assign to object df
 df = pd.read_csv(filepath+csvfile,index_col=0,encoding='utf-8',nrows=numrows)
 
-
 #print dataframe
 print(df)
+
+mp.plot([1,2,3,4],[1,2,5,12])
+mp.xlabel('x numbers')
+mp.ylabel('some numbers')
+mp.show()
+
+
+
+
 
