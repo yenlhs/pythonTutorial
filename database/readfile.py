c78263b56dc72d83f6634ff0f58826fb7ae5b39f
Adrian <yenlhs@gmail.com>
2018-01-26 20:15:29 +1100

    create a readfile python script using pandas and numpy library

---
+++ database/readfile.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+
+#change this variable to adjust the number of rows to display. 
+#Write None if you wish to display all rows
+numrows = 3
+
+#path to csv file 
+filepath = '/Users/Adrian/Documents/pythonTutorial/database/'
+#name of csv file
+csvfile = 'test.csv'
+
+#read csv file and assign to object df
+df = pd.read_csv(filepath+csvfile,index_col=0,encoding='utf-8',nrows=numrows)
+
+
+#print dataframe
+print(df)
